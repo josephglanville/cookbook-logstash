e404e50dcc36df6088be1c5ff1d9ded7548c56b9
Joseph Glanville <jpg@jpg.id.au>
2014-12-22 10:34:18 +1100

    Disable logstash-web

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,3 +1,7 @@
-service 'logstash' do
-  action :enable
+# Included logstash-web is nasty.
+# If you want kibana install it behind a proper web server
+# This disables it from starting.
+file '/etc/init/logstash-web.override' do
+  content 'manual'
+  mode 0644
 end
