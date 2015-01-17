ec63ae325eab8a886846d7a48acb1821ee707beb
Joseph Glanville <jpg@jpg.id.au>
2015-01-17 11:53:36 +1100

    Drop support for dpkg install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,1 @@
-default['logstash']['install_method'] = 'repo'
 default['logstash']['home_dir'] = '/var/lib/logstash'
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,23 +1,8 @@
 include_recipe 'logstash::java'
+include_recipe 'logstash::repo'
 
-case node['logstash']['install_method']
-when 'repo'
-  include_recipe 'logstash::repo'
-  %w(logstash logstash-contrib).each do |pkg|
-    package pkg
-  end
-when 'dpkg'
-  local_path = ::File.join(Chef::Config[:file_cache_path],
-                           node['logstash']['dpkg']['filename'])
-  remote_file local_path do
-    source "#{node['logstash']['dpkg']['location']}/#{node['logstash']['dpkg']['filename']}"
-  end
-
-  dpkg_package 'logstash' do
-    source local_path
-  end
-else
-  fail "Unsupported Logstash installation method"
+%w(logstash logstash-contrib).each do |pkg|
+  package pkg
 end
 
 cookbook_file '/etc/logrotate.d/logstash' do
